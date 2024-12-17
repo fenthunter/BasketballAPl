10f3045832ff6754f73269c61a58918c80158bac
Hunter Korczykowski <hunterkorczykowski@gmail.com>
2024-12-17 14:35:48 -0500

    Initial Commit

---
+++ BasketballAPl/BasketballAPlApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasketballAPlApp.swift
+//  BasketballAPl
+//
+//  Created by Hunter Korczykowski (student LM) on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BasketballAPlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
