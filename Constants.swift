4fc67388cf955522eb83d1bc732e7737a10f66f8
Esan Yi <esanbluepizza@gmail.com>
2025-01-07 13:40:09 -0500

    Testing

---
+++ Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  BasketballAPl
+//
+//  Created by Esan Yi (student LM) on 1/2/25.
+//
+
+import SwiftUI
+
+struct Constants: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Constants()
+}
---
+++ ViewState.swift
@@ -0,0 +1,8 @@
+//
+//  ViewState.swift
+//  BasketballAPl
+//
+//  Created by Esan Yi (student LM) on 1/2/25.
+//
+
+import Foundation
